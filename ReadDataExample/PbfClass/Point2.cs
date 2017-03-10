5178abe4209ad00bdf33d67af129e0e2d5968946
ingen084 <ingen188@gmail.com>
2017-03-10 22:26:11 +0900

    サンプルのPoint2クラスを使いやすくした

--- ReadDataExample/PbfClass/Point2.cs
+++ ReadDataExample/PbfClass/Point2.cs
@@ -5,6 +5,12 @@ namespace ReadDataExample.PbfClass
 	[ProtoContract]
 	public struct Point2
 	{
+		public Point2(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+
 		[ProtoMember(1)]
 		public int X { get; set; }
 
@@ -14,17 +20,53 @@ namespace ReadDataExample.PbfClass
 		public override string ToString()
 			=> $"X:{X} Y:{Y}";
 
-		//TODO Formで使う場合はここのコメントアウトを外すと自動でキャストしてくれるようになります。
+		public static Point2 operator +(Point2 point, int num)
+			=> new Point2(point.X + num, point.Y + num);
+
+		public static Point2 operator -(Point2 point, int num)
+			=> new Point2(point.X - num, point.Y - num);
+
+		public static Point2 operator +(Point2 point1, Point2 point2)
+			=> new Point2(point1.X + point2.X, point1.Y + point2.Y);
+
+		public static Point2 operator -(Point2 point1, Point2 point2)
+			=> new Point2(point1.X - point2.X, point1.Y - point2.Y);
+
+		public static Point2 operator *(Point2 point, int num)
+			=> new Point2(point.X * num, point.Y * num);
+
+		public static Point2 operator /(Point2 point, int num)
+			=> new Point2(point.X / num, point.Y / num);
+
+		public static Point2 operator *(Point2 point1, Point2 point2)
+			=> new Point2(point1.X * point2.X, point1.Y * point2.Y);
+
+		public static Point2 operator /(Point2 point1, Point2 point2)
+			=> new Point2(point1.X / point2.X, point1.Y / point2.Y);
+
+		public static bool operator ==(Point2 point1, Point2 point2)
+			=> point1.X == point2.X && point1.Y == point2.Y;
+
+		public static bool operator !=(Point2 point1, Point2 point2)
+			=> point1.X != point2.X || point1.Y != point2.Y;
+
+		public override bool Equals(object obj)
+			=> base.Equals(obj);
+
+		public override int GetHashCode()
+			=> base.GetHashCode();
+
+		//NOTE Form(System.Drawing)や、WPF(System.Windows)で使う場合はここのコメントアウトを外すと自動でキャストしてくれるようになります。
 		/*
 		public static implicit operator Point2(Point point)
 			=> new Point2(point.X, point.Y);
 		public static implicit operator Point(Point2 point)
-			=> new Point((int)point.X, (int)point.Y);
+			=> new Point(point.X, point.Y);
 
 		public static implicit operator Point2(Size point)
 			=> new Point2(point.Width, point.Height);
 		public static implicit operator Size(Point2 point)
-			=> new Size((int)point.X, (int)point.Y);
+			=> new Size(point.X, point.Y);
 		*/
 	}
 }
